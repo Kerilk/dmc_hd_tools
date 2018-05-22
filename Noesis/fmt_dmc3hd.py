8211c52847b140b578854e8dab6e2a62372b44b2
Brice Videau <brice.videau@free.fr>
2018-05-22 20:06:10 +0200

    Should be working correctly, thanks to Fatduck's importer.

--- Noesis/fmt_dmc3hd.py
+++ Noesis/fmt_dmc3hd.py
@@ -19,23 +19,70 @@ class Batch:
 		self.offsetPositions = bs.readUInt64()
 		self.offsetNormals = bs.readUInt64()
 		self.offsetUVs = bs.readUInt64()
-		self.offsetUnknown1 = bs.readUInt64()
-		self.offsetUnknown2 = bs.readUInt64()
+		self.offsetBoneIndexes = bs.readUInt64()
+		self.offsetBoneWeights = bs.readUInt64()
 		self.padding2 = bs.readBytes(8)
 		self.unknown = bs.readUInt64()
 		self.padding3 = bs.readBytes(8)
-		self.positions = []
-		self.normals = []
-		self.uvs = []
+		self.positions = ""
+		self.normals = ""
+		self.boneIndexes = ""
+		self.boneWeights = ""
+		self.uvs = ""
+		self.triangles = ""
+		self.trianglesIndexCount = 0
 
 	def parseVertices(self):
 		bs = self.bs
 		bs.seek(self.offsetPositions, NOESEEK_ABS)
 		self.positions = bs.readBytes(self.numVertices * 3 * 4)
+
 		bs.seek(self.offsetNormals, NOESEEK_ABS)
 		self.normals = bs.readBytes(self.numVertices * 3 * 4)
+
 		bs.seek(self.offsetUVs, NOESEEK_ABS)
-		self.uvs = bs.readBytes(self.numVertices * 2 * 2)
+		self.uvs = []
+		for i in range(self.numVertices):
+			self.uvs.append( bs.readShort()/4096.0 )
+			self.uvs.append( 1.0 - bs.readShort()/4096.0 )
+		self.uvs = struct.pack(str(len(self.uvs))+"f", *self.uvs)
+
+
+		bs.seek(self.offsetBoneIndexes, NOESEEK_ABS)
+		bI = []
+		for i in range(self.numVertices):
+			bs.readByte()
+			bI.append( bs.readByte()//4 )
+			bI.append( bs.readByte()//4 )
+			bI.append( bs.readByte()//4 )
+		self.boneIndexes = struct.pack(str(len(bI))+"b",*bI)
+
+		bs.seek(self.offsetBoneWeights, NOESEEK_ABS)
+		boneWeights = []
+		triangleSkip = []
+		for i in range(self.numVertices):
+			w = bs.readUShort()
+			triangleSkip.append( (w >> 15) & 1 )
+			boneWeights.append( ((w >> 10) & 0x1f) / 31.0 )
+			boneWeights.append( ((w >>  5) & 0x1f) / 31.0 )
+			boneWeights.append( ((w >>  0) & 0x1f) / 31.0 )
+		self.boneWeights = struct.pack("<"+str(len(boneWeights))+"f",*bI)
+
+		triangles = []
+		v1 = 0
+		v2 = 1
+		for i in range(2, self.numVertices):
+			v3 = i
+			if triangleSkip[i] == 0:
+				triangles.append(v2)
+				triangles.append(v1)
+				triangles.append(v3)
+			v1 = v2
+			v2 = v3
+		self.triangles = struct.pack("<"+str(len(triangles))+"H",*triangles)
+		self.trianglesIndexCount = len(triangles)
+
+
 		return 1
 
 
@@ -66,6 +113,49 @@ class Mesh:
 		for b in self.batches:
 			b.parseVertices()
 
+class BoneStruct:
+	def __init__(self, bs, numBones):
+		base_offset = bs.tell()
+		self.bs = bs
+		self.numBones = numBones
+		self.offsetHierarchy = bs.readUInt()
+		self.offsetHierarchyOrder = bs.readUInt()
+		self.offsetUnknown = bs.readUInt()
+		self.offsetMatrices = bs.readUInt()
+
+		bs.seek(base_offset + self.offsetHierarchy, NOESEEK_ABS)
+		self.hierarchy = []
+		for i in range(numBones):
+			self.hierarchy.append( bs.readByte() )
+
+		bs.seek(base_offset + self.offsetHierarchyOrder, NOESEEK_ABS)
+		self.hierarchyOrder = []
+		for i in range(numBones):
+			self.hierarchyOrder.append( bs.readByte() )
+
+		bs.seek(base_offset + self.offsetUnknown, NOESEEK_ABS)
+		self.unknown = []
+		for i in range(numBones):
+			self.unknown.append( bs.readByte() )
+
+		bs.seek(base_offset + self.offsetMatrices, NOESEEK_ABS)
+		self.matrices = []
+		for i in range(numBones):
+			self.matrices.append( bs.readBytes(32) )
+
+		parents = []
+		for i in range(numBones):
+			parents.append(-1)
+		for i in range(numBones):
+			parents[self.hierarchyOrder[i]] = self.hierarchy[i]
+
+		self.bones = []
+		for i in range(numBones):
+			mat = NoeMat43()
+			mat = mat.translate(NoeVec3.fromBytes(self.matrices[i]))
+			self.bones.append( NoeBone(i, "bone%03i"%i, mat, None, parents[i]) )
+		self.bones = rapi.multiplyBones(self.bones)
+
 class ModFile:
 	def __init__(self, bs):
 		self.bs = bs
@@ -77,6 +167,7 @@ class ModFile:
 		self.unknown2 = 0
 		self.offsetBoneStruct = 0
 		self.meshes = []
+		self.boneStruct = None
 
 	def parseHeader(self):
 		bs = self.bs
@@ -116,7 +207,10 @@ class ModFile:
 			m.parseVertices()
 		return 1
 
-
+	def parseBones(self):
+		bs = self.bs
+		bs.seek(self.offsetBoneStruct, NOESEEK_ABS)
+		self.boneStruct = BoneStruct(bs, self.numBones)
 
 
 def modCheckType(data):
@@ -126,30 +220,32 @@ def modCheckType(data):
 	return 1
 
 def modLoadModel(data, mdlList):
-	noesis.logPopup()
+	#noesis.logPopup()
 	mod = ModFile(NoeBitStream(data))
 	if mod.parseHeader() == 0:
 		return 0
 	mod.parseMeshes()
 	mod.parseBatches()
 	mod.parseVertices()
+	mod.parseBones()
 	ctx = rapi.rpgCreateContext()
 	rapi.rpgSetOption(noesis.RPGOPT_BIGENDIAN, 0)
 	i = 0
 	for m in mod.meshes:
 		j = 0
 		for b in m.batches:
-			print(str(i) + "-" + str(j))
+			#print(str(i) + "-" + str(j))
 			rapi.rpgSetName(str(i) + "-" + str(j))
 			rapi.rpgBindPositionBuffer(b.positions, noesis.RPGEODATA_FLOAT, 12)
 			rapi.rpgBindNormalBuffer(b.normals, noesis.RPGEODATA_FLOAT, 12)
-			rapi.rpgBindUV1Buffer(b.uvs, noesis.RPGEODATA_USHORT, 4)
-			indexes = list(range(b.numVertices))
-			triangles = struct.pack("<"+str(len(indexes))+"H",*indexes)
-			rapi.rpgCommitTriangles(triangles, noesis.RPGEODATA_USHORT, len(indexes), noesis.RPGEO_TRIANGLE_STRIP, 1)
+			rapi.rpgBindUV1Buffer(b.uvs, noesis.RPGEODATA_FLOAT, 8)
+			rapi.rpgBindBoneIndexBuffer(b.boneIndexes, noesis.RPGEODATA_UBYTE, 3, 3)
+			rapi.rpgBindBoneWeightBuffer(b.boneWeights, noesis.RPGEODATA_FLOAT, 12, 3)
+			rapi.rpgCommitTriangles(b.triangles, noesis.RPGEODATA_USHORT, b.trianglesIndexCount, noesis.RPGEO_TRIANGLE, 1)
 			rapi.rpgClearBufferBinds()
 			j = j + 1
 		i = i + 1
-	mdl = rapi.rpgConstructModelSlim()
+	mdl = rapi.rpgConstructModel()
+	mdl.setBones(mod.boneStruct.bones)
 	mdlList.append(mdl)
 	return 1
